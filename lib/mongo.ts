9dee5d707a6bec71c5beb881e49a0b211c305b12
kibudi <shahar002000@gmail.com>
2025-09-15 10:46:27 +0300

    xx

--- lib/mongo.ts
+++ lib/mongo.ts
@@ -1,5 +1,14 @@
 // lib/mongodb.ts
-import mongoose from "mongoose";
+import mongoose, { Mongoose } from "mongoose";
+
+interface Cached {
+  conn: Mongoose | null;
+  promise: Promise<Mongoose> | null;
+}
+
+declare global {
+  var mongoose: Cached | undefined;
+}
 
 const MONGODB_URI = process.env.MONGODB_URI as string;
 
@@ -7,16 +16,18 @@ if (!MONGODB_URI) {
   throw new Error("⚠️ Please define MONGODB_URI in .env.local");
 }
 
-const cached = (global as any).mongoose || { conn: null, promise: null };
+const cached: Cached = global.mongoose || { conn: null, promise: null };
 
-export async function connectToDatabase() {
+export async function connectToDatabase(): Promise<Mongoose> {
   if (cached.conn) return cached.conn;
 
   if (!cached.promise) {
     cached.promise = mongoose
       .connect(MONGODB_URI, { dbName: "portfolio" })
-      .then((mongoose) => mongoose);
+      .then((m) => m);
   }
+
   cached.conn = await cached.promise;
+  global.mongoose = cached; 
   return cached.conn;
 }
